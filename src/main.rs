0a70c0f4d4a8b3046c434dee12c82c28519439ca
Jonathan Nakandala <Jonathannakandala@gmail.com>
2020-01-30 23:51:15 +0000

    Made previous tutorials functions and did the generate a triangle exercise

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
+use image::ImageBuffer;
+use image::Rgba;
 use std::sync::Arc;
-
-use image::{ImageBuffer, Rgba};
 use vulkano::buffer::BufferUsage;
 use vulkano::buffer::CpuAccessibleBuffer;
 use vulkano::command_buffer::AutoCommandBufferBuilder;
 use vulkano::command_buffer::CommandBuffer;
+use vulkano::command_buffer::DynamicState;
 use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
 use vulkano::device::Device;
 use vulkano::device::DeviceExtensions;
 use vulkano::format::ClearValue;
 use vulkano::format::Format;
+use vulkano::framebuffer::Framebuffer;
+use vulkano::framebuffer::Subpass;
 use vulkano::image::Dimensions;
 use vulkano::image::StorageImage;
 use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
 use vulkano::instance::PhysicalDevice;
+use vulkano::pipeline::viewport::Viewport;
 use vulkano::pipeline::ComputePipeline;
+use vulkano::pipeline::GraphicsPipeline;
 use vulkano::sync::GpuFuture;
 
-fn main() {
+fn create_device() -> (
+    std::sync::Arc<vulkano::device::Device>,
+    std::sync::Arc<vulkano::device::Queue>,
+) {
     let instance =
         Instance::new(None, &InstanceExtensions::none(), None).expect("failed to create instance");
     let physical = PhysicalDevice::enumerate(&instance)
@@ -53,6 +61,13 @@ fn main() {
     };
     let queue = queues.next().unwrap();
 
+    (device, queue)
+}
+
+fn copy_image_to_buffer(
+    device: std::sync::Arc<vulkano::device::Device>,
+    queue: std::sync::Arc<vulkano::device::Queue>,
+) {
     let source_content = 0..64;
     let source = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), source_content)
         .expect("failed to create buffer");
@@ -77,7 +92,12 @@ fn main() {
     assert_eq!(&*src_content, &*dest_content);
 
     println!("Copied the contents of one CpuAccessibleBuffer into another!");
+}
 
+fn multiply_lots_of_values(
+    device: std::sync::Arc<vulkano::device::Device>,
+    queue: std::sync::Arc<vulkano::device::Queue>,
+) {
     let data_iter = 0..16384;
     let data_buffer = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), data_iter)
         .expect("failed to create buffer");
@@ -137,7 +157,12 @@ fn main() {
     }
 
     println!("Ran a shader that multiplied by 12 every value in a list of 16384!");
+}
 
+fn generate_solid_color_image(
+    device: std::sync::Arc<vulkano::device::Device>,
+    queue: std::sync::Arc<vulkano::device::Queue>,
+) {
     let image = StorageImage::new(
         device.clone(),
         Dimensions::Dim2d {
@@ -178,5 +203,168 @@ fn main() {
 
     let buffer_content = cpubuf.read().unwrap();
     let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
-    image.save("image.png").unwrap();
+    image.save("solidcolour.png").unwrap();
+}
+
+fn main() {
+    let (device, queue) = create_device();
+    copy_image_to_buffer(device.clone(), queue.clone());
+    multiply_lots_of_values(device.clone(), queue.clone());
+    generate_solid_color_image(device.clone(), queue.clone());
+
+    let image = StorageImage::new(
+        device.clone(),
+        Dimensions::Dim2d {
+            width: 1024,
+            height: 1024,
+        },
+        Format::R8G8B8A8Unorm,
+        Some(queue.family()),
+    )
+    .unwrap();
+
+    let read_buffer = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage::all(),
+        (0..1024 * 1024 * 4).map(|_| 0u8),
+    )
+    .expect("failed to create buffer");
+
+    #[derive(Default, Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+    vulkano::impl_vertex!(Vertex, position);
+
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+    };
+    let vertex_buffer = CpuAccessibleBuffer::from_iter(
+        device.clone(),
+        BufferUsage::all(),
+        vec![vertex1, vertex2, vertex3].into_iter(),
+    )
+    .unwrap();
+
+    let render_pass = Arc::new(
+        vulkano::single_pass_renderpass!(device.clone(),
+            attachments: {
+                color: {
+                    load: Clear,
+                    store: Store,
+                    format: Format::R8G8B8A8Unorm,
+                    samples: 1,
+                }
+            },
+            pass: {
+                color: [color],
+                depth_stencil: {}
+            }
+        )
+        .unwrap(),
+    );
+
+    let framebuffer = Arc::new(
+        Framebuffer::start(render_pass.clone())
+            .add(image.clone())
+            .unwrap()
+            .build()
+            .unwrap(),
+    );
+
+    mod vs_triangle {
+        vulkano_shaders::shader! {
+            ty: "vertex",
+            src: "
+            #version 450
+            
+            layout(location = 0) in vec2 position;
+            
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0);
+            }
+            "
+        }
+    }
+
+    mod fs_triangle {
+        vulkano_shaders::shader! {
+            ty: "fragment",
+            src:"
+            #version 450
+
+            layout(location = 0) out vec4 f_color;
+            
+            void main() {
+                f_color = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+            "
+        }
+    }
+
+    let vs_triangle = vs_triangle::Shader::load(device.clone())
+        .expect("failed to create vs_triangle shader module");
+    let fs_triangle = fs_triangle::Shader::load(device.clone())
+        .expect("failed to create fs_triangle shader module");
+
+    let pipeline = Arc::new(
+        GraphicsPipeline::start()
+            .vertex_input_single_buffer::<Vertex>()
+            .vertex_shader(vs_triangle.main_entry_point(), ())
+            .viewports_dynamic_scissors_irrelevant(1)
+            .fragment_shader(fs_triangle.main_entry_point(), ())
+            .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+            .build(device.clone())
+            .unwrap(),
+    );
+
+    let dynamic_state = DynamicState {
+        viewports: Some(vec![Viewport {
+            origin: [0.0, 0.0],
+            dimensions: [1024.0, 1024.0],
+            depth_range: 0.0..1.0,
+        }]),
+        ..DynamicState::none()
+    };
+
+    let command_buffer =
+        AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family())
+            .unwrap()
+            .begin_render_pass(
+                framebuffer.clone(),
+                false,
+                vec![[0.0, 0.0, 1.0, 1.0].into()],
+            )
+            .unwrap()
+            .draw(
+                pipeline.clone(),
+                &dynamic_state,
+                vertex_buffer.clone(),
+                (),
+                (),
+            )
+            .unwrap()
+            .end_render_pass()
+            .unwrap()
+            .copy_image_to_buffer(image.clone(), read_buffer.clone())
+            .unwrap()
+            .build()
+            .unwrap();
+
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    finished
+        .then_signal_fence_and_flush()
+        .unwrap()
+        .wait(None)
+        .unwrap();
+
+    let buffer_content = read_buffer.read().unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    image.save("triangle.png").unwrap();
 }
